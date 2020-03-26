460b615a81798c6473497115c4307c3949327f15
benchu1k <benchu1k@gmail.com>
2020-03-27 07:57:33 +0800

    1

--- instance.go
+++ instance.go
@@ -2,60 +2,82 @@ package main
 
 import (
 	"fmt"
+	"go/types"
 	"log"
 	"net"
 )
 
+type socket_type uint8
+
+const (
+	UDP        socket_type = 0
+	TCP        socket_type = 1
+	WEB_SOCKET socket_type = 2
+)
+
 type SocketProcess interface {
-	Listen(s *Server) (net.Conn, error)
-	OnStartError(err error)
-	Read(conn net.Conn) (string, interface{}, error)
-	Write(conn net.Conn)
+	Listen(*Server) (*Conn, error)
+	OnStartError(error)
+	Read(net.Conn) (string, interface{}, error)
+	Write(net.Conn)
 }
 
-func Process(s *Server, sp SocketProcess) {
-	conn, err := sp.Listen(s)
-	if err != nil {
-		log.Printf("ID: %d TYPE: %s: ERROR: %s", s.id, s.name, err)
-		sp.OnStartError(err)
-	}
-	client := Client{id, &conn}
-	append(clients, &client)
-	id = id + 1
-	fmt.Println("%s服务器端启动:  %s", s.name, addr.String())
-	defer s.OnClose(&client)
-	defer conn.Close()
+type ConnRW interface {
+	Read() ([]byte, error)
+	Write([]byte) error
+}
+
+type Conn struct {
+	conn interface{}
+}
+
+func Process(s *Server) {
+	w := &Worker{s.sType, run}
 	for {
-		mt, message, err := sp.Read(conn)
+		conn, err := w.Listen(s.addr)
 		if err != nil {
-			s.OnError(&client)
+			log.Printf("ID: %d TYPE: %s: ERROR: %s", s.id, s.name, err)
 		}
-		s.OnMessage(&client, message)
+		client := &Client{id, conn}
+		clients = append(clients, client)
+		id = id + 1
+		fmt.Println("%s服务器端启动:  %s", s.name, s.addr)
+		defer s.OnClose(client)
+		defer conn.Close()
+		for {
+			message, err := conn.Read()
+			if err != nil {
+				s.OnError(client, err)
+			}
+			s.OnMessage(client, message)
+		}
 	}
 }
 
-type Onstart func()
-type OnOpen func()
-type OnConnect func(cli *Client)
-type OnMessage func(cli *Client, msg interface{})
-type OnError func(cli *Client)
-type OnClose func(cli *Client)
+type Worker struct {
+	sType socket_type
+	run   func()
+}
 
 var (
 	id      int64 = 0
 	clients []*Client
+	servers []*Server
 )
 
 type Client struct {
 	id   int64
-	conn *net.Conn
+	conn *Conn
 }
 
 type Server struct {
 	id        int64
-	name      string
-	addr      net.Addr
-	OnMessage OnMessage
-	OnError   OnError
-	OnClose   OnClose
+	sType     socket_type
+	addr      string
+	OnMessage func(*Client, []byte)
+	OnError   func(error)
+	OnClose   func(*Client)
+	OnConnect func(*Client)
+	OnStart   func()
+	OnOpen    func()
 }
--- tcp.go
+++ tcp.go
@@ -3,21 +3,56 @@ package main
 import (
 	"fmt"
 	"net"
+	"time"
 )
 
-func tcp() {
-	addr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:18880")
-	dealErrorWithExist(err)
+type TCPWorker struct {
+}
+
+func (w *TCPWorker) Process(s *Server) {
+	addr, err := net.ResolveTCPAddr("tcp", s.addr)
+	s.OnError(err)
 	tcpListen, err := net.ListenTCP("tcp", addr)
-	dealErrorWithExist(err)
+	s.OnError(err)
 	fmt.Println("tcp服务器端启动:  ", addr.String())
 	for {
 		conn, err := tcpListen.Accept()
 		if err != nil {
 			continue
 		}
-		handlerConn(conn)
-		conn.Close() //正常链接情况下，handlerConn不会释放出来到这里 当客户端强制断开，才会return到这里，吧当前conn关闭
-		fmt.Println("当前用户 ", conn.RemoteAddr(), " 主动断开链接！")
+		c := &Conn{}
+		client := &Client{id, c}
+		clients = append(clients, client)
+		var buf [1024]byte
+		for {
+			readSize, err := conn.Read(buf[0:])
+			s.OnError(err)
+			remoteAddr := conn.RemoteAddr()
+			fmt.Println("来自远程ip:", remoteAddr, " 的消息:", string(buf[0:readSize]))
+			_, err2 := conn.Write([]byte(string(buf[0:readSize]) + " " + time.Now().String()))
+			//一定要执行下面的return 才能监听到客户端主动断开，服务器端对本次conn进行close处理 dealErrorWithReturn不能达到这个效果。
+			if err2 != nil {
+				fmt.Println("当前用户 ", conn.RemoteAddr(), " 主动断开链接！")
+				conn.Close() //正常链接情况下，handlerConn不会释放出来到这里 当客户端强制断开，才会return到这里，吧当前conn关闭
+				s.OnClose(client)
+				return
+			}
+		}
 	}
+}
+
+func (c *Conn) Read() ([]byte, error) {
+	var buf [1024]byte
+	readSize, err := c.conn.Read(buf[0:])
+	if err != nil {
+		return buf[0:], err
+	}
+	return buf[0:readSize], nil
+}
+func (c *Conn) Write(b []byte) error {
+	_, err := c.conn.Write(b)
+	if err != nil {
+		return err
+	}
+	return nil
 }
